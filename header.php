c2b7733613321f863dd8f64668bd4dfdb9af5a2b
Bob D'Errico <bob@bobderrico.com>
2015-02-02 00:04:33 -0500

    Added Google Analytics tracking code

--- header.php
+++ header.php
@@ -32,6 +32,16 @@
     <?php wp_head(); ?>
     <meta name="viewport" content="width=device-width"/>
     <meta name="description" content="We are a full service family salon, with a fun atmosphere. We offer the latest styles in haircutting, texturizing, coloring, hylights/lowlights, and more.">
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-59226829-1', 'auto');
+          ga('send', 'pageview');
+
+    </script>
   </head>
   <body>
     <div class="container">
