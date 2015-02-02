030c5ef58f9889f9dcb2c768b9aa954614df2630
Bob D'Errico <bob@bobderrico.com>
2015-02-01 21:57:13 -0500

    Updated and added slideshow pictures

--- front-page.php
+++ front-page.php
@@ -6,6 +6,13 @@ get_header();
         <img src="<?php echo get_template_directory_uri(); ?>/slides/slide01.jpg"/>
         <img src="<?php echo get_template_directory_uri(); ?>/slides/slide02.jpg"/>
         <img src="<?php echo get_template_directory_uri(); ?>/slides/slide03.jpg"/>
+        <img src="<?php echo get_template_directory_uri(); ?>/slides/slide04.jpg"/>
+        <img src="<?php echo get_template_directory_uri(); ?>/slides/slide05.jpg"/>
+        <img src="<?php echo get_template_directory_uri(); ?>/slides/slide06.jpg"/>
+        <img src="<?php echo get_template_directory_uri(); ?>/slides/slide07.jpg"/>
+        <img src="<?php echo get_template_directory_uri(); ?>/slides/slide08.jpg"/>
+        <img src="<?php echo get_template_directory_uri(); ?>/slides/slide09.jpg"/>
+        <img src="<?php echo get_template_directory_uri(); ?>/slides/slide10.jpg"/>
       </div>
       <main>
         <?php 
